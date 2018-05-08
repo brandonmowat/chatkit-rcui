f73f9f2acd90541d7058c00ecb3877c87c662f93
Brandon Mowat <me@brandonmowat.com>
2018-05-07 23:45:26 -0400

    Fixes compilation err

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { ChatManager, TokenProvider } from '@pusher/chatkit'
-import { ChatFeed } from 'react-chat-ui'
+import { ChatFeed, Message } from 'react-chat-ui'
 
 export default class App extends Component {
 
-	constructor() {
-		const chatManager = new ChatManager({
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			messages: [
+				new Message({
+		      id: 1,
+		      message: "I'm the recipient! (The person you're talking to)",
+		    })
+			]
+		}
+
+		this.chatManager = new ChatManager({
 		 	instanceLocator: 'v1:us1:cb86d714-ead7-42a2-aece-f1f647c3cd2c',
-		  userId: 'sarah',
-		  tokenProvider: new TokenProvider({ url: 'your.auth.url' })
+		  userId: 'Brandon',
+		  tokenProvider: new TokenProvider({ url: 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/:instance_id' })
 		})
 	}
 
@@ -17,7 +28,12 @@ export default class App extends Component {
 	}
 
 	render() {
-		return <h1>hey</h1>
+		return (<ChatFeed
+      messages={this.state.messages} // Boolean: list of message objects
+      hasInputField={false} // Boolean: use our input, or use your own
+      showSenderName // show the name of the user who sent the message
+      bubblesCentered={false} //Boolean should the bubbles be centered in the feed?
+    />)
 	}
 
 }
